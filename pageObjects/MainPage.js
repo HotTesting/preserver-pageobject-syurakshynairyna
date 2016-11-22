14db17daad470ba71c98fcac7addf366e95fb85a
syurakshynairyna <iryna.s@trip-top.com>
2016-11-22 02:01:53 +0200

    Add files via upload

--- pageObjects/MainPage.js
+++ pageObjects/MainPage.js
@@ -1,24 +1,19 @@
-class NotesPage {
+class MainPage {
 
-    constructor() {
+        constructor() {
         this.newNoteBodyField = $('.note-editor textarea')
         this.newNoteTitleField = $('.note-editor input[placeholder="Title"]')
     }
 
     //Создаст заметку на странице
-    createNote(title, body) {
+    createNotes(title, body) {
         this.newNoteBodyField.click()
         this.newNoteBodyField.sendKeys(body)
         this.newNoteTitleField.click()
         this.newNoteTitleField.sendKeys(title)
         element(by.buttonText('Save')).click()
     }
-
-    //Получим коллекцию всех заметок которые есть на этой странице
-    getNotes() {
-        return $$('.grid-container .grid-item')
-    }
 }
 
 // Экспортим объект чтобы он был доступен в других файлах
-module.exports.NotesPage = NotesPage
+module.exports.MainPage = MainPage
